713d3a2ddb494f60f101b98c5de20bbb96702bd9
abrarishere <153655587+abrarishere@users.noreply.github.com>
2023-12-15 08:28:21 +0000

    pull

---
+++ reverse name.py
@@ -0,0 +1,8 @@
+# Get the input string from the user
+string = input("Enter a string: ")
+
+# Reverse the string using slicing
+reversed_string = string[::-1]
+
+# Print the reversed string
+print("Reversed string:", reversed_string)
